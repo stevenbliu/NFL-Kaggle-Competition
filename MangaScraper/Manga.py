a15ce2334f0b1cf81fd874212fc623ec3a74fdac
sdevinl <45984322+sdevinl@users.noreply.github.com>
2019-07-19 01:54:58 -0700

    Add files via upload

--- MangaScraper/Manga.py
+++ MangaScraper/Manga.py
@@ -25,11 +25,17 @@ url = 'https://www.mangareader.net/'
 links = {}
 chapters = {}
 
-web = 'https://www.mangareader.net/nanatsu-no-taizai'
+manga_name = input('Enter the name of your manga with spaces: ')
+print(manga_name)
+
+manga_name = manga_name.replace(' ', '-')
+manga_name = manga_name.lower()
+web = url + manga_name
+
 response = requests.get(web)
 soup = BeautifulSoup(response.content, 'html.parser')
-
 img_tags = soup.find_all('a')
+
 
 # show images
 # img = Image.open(BytesIO(response.content))
@@ -43,19 +49,36 @@ links[series] = []
 for i in img_tags:
     chapters[series].append(i.contents)
     links[series].append(i['href'])
-print('links:',links)
+#print('links:', links)
 
 series_format = series.replace(' ', '-').lower()
-print('series:',series_format)
+#print('series:', series_format)
 
 filtered_links = filter(lambda x: series_format in x, links[series])
 filtered_links = list(filtered_links)
 
-temp_url = url + filtered_links[4][1:]
-print(url,filtered_links[4][1:])
+latest_chapter = re.findall(r'\d+', filtered_links[0])
+latest_chapter = latest_chapter[0]
+print('Latest Chapter:', latest_chapter)
+
+chosen_chapter = input('Please select the chapter you want to read: ')
+print('You have selected chapter', chosen_chapter)
+
+chosen_chapter = int(chosen_chapter)
+
+temp_url = url + filtered_links[chosen_chapter + 5][1:]
+
+
 print('temp_url:', temp_url)
+
+
+
+response = requests.get(web)
+soup = BeautifulSoup(response.content, 'html.parser')
+img_tags = soup.find_all('a')
+
 temp_response = requests.get(temp_url)
-temp_soup = BeautifulSoup(temp_url, 'html.parser')
+temp_soup = BeautifulSoup(urlopen(temp_url), 'html.parser')
 temp_img_tag = soup.src
 
 print('src', temp_img_tag)
@@ -63,4 +86,5 @@ print('src', temp_img_tag)
 #img = Image.open(BytesIO(temp_img_tag))
 #img.show()
 
-webbrowser.open_new_tab(temp_url)
+
+#webbrowser.open_new_tab(temp_url)
